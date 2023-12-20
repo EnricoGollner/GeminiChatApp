ce12f7164d2368ec639bb9af36b14aa8926bae7a
Lucas de Sena Bastos <lucasdesena@ucl.br>
2023-12-20 12:44:32 -0300

    adicionando variaveis de ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:chat_bot_app/src/chat_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 
 final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
     GlobalKey<ScaffoldMessengerState>();
 
-void main() {
-  Gemini.init(apiKey: 'AIzaSyDxcLVQbF3IuJFd9xvsVXlVQqoFLCiJTgU');
-
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  String apiKey = dotenv.env['API_KEY']!;
+  Gemini.init(apiKey: apiKey);
   runApp(const ChatApp());
 }
