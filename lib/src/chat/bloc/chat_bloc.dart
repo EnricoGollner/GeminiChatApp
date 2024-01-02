e831ea9a345907e81617923cc8371d93a467a6ba
Lucas de Sena Bastos <lucasdesena@ucl.br>
2024-01-02 12:13:10 -0300

    Ajustando chat para ter contexto(Multi-turn conversations)

--- lib/src/chat/bloc/chat_bloc.dart
+++ lib/src/chat/bloc/chat_bloc.dart
@@ -9,18 +9,23 @@ import 'package:flutter_gemini/flutter_gemini.dart';
 class ChatBloc {
   // final ChatRepository _chatRepository;
 
-  final StreamController<ChatEvent> _chatInputController = StreamController<ChatEvent>();
-  final StreamController<ChatState> _chatOutputController = StreamController<ChatState>();
+  final StreamController<ChatEvent> _chatInputController =
+      StreamController<ChatEvent>();
+  final StreamController<ChatState> _chatOutputController =
+      StreamController<ChatState>();
 
   Sink<ChatEvent> get chatInputSink => _chatInputController.sink;
   Stream<ChatState> get chatOutputStream => _chatOutputController.stream;
 
-  ChatBloc(/*{required ChatRepository chatRepository}*/) {
-    _chatInputController.stream.listen((event) async {
-      await _mapEventToState(event);
-    },);
-  }
+  List<Content> chat = [];
 
+  ChatBloc(/*{required ChatRepository chatRepository}*/) {
+    _chatInputController.stream.listen(
+      (event) async {
+        await _mapEventToState(event);
+      },
+    );
+  }
 
   Future<void> _mapEventToState(ChatEvent event) async {
     // List<PromptChat> chatHistory = await _chatRepository.getChatHistory();
@@ -28,22 +33,38 @@ class ChatBloc {
 
     if (event is LoadChatEvent) {
       _chatOutputController.add(ChatLoadingState(chatHistory: chatHistory));
-      
-      await Gemini.instance.text('Olá!').then(
-        (response) => chatHistory.add(PromptChat(sender: MessageSender.gemini, message: response?.output ?? ''))
-      );
+
+      await Gemini.instance.text('Olá!').then((response) => chatHistory.add(
+          PromptChat(
+              sender: MessageSender.gemini, message: response?.output ?? '')));
     } else if (event is SendMessageChatEvent) {
-      _chatOutputController.add(ChatLoadingState(chatHistory: event.chatHistory));
+      _chatOutputController
+          .add(ChatLoadingState(chatHistory: event.chatHistory));
 
       chatHistory = event.chatHistory;
       chatHistory.add(event.message);
-      
-      await Gemini.instance.text(event.message.message).then(
-        (response) => chatHistory.add(PromptChat(sender: MessageSender.gemini, message: response?.output ?? ''))
+
+      chat.add(
+        Content(
+          parts: [Parts(text: event.message.message)],
+          role: 'user',
+        ),
       );
+
+      await Gemini.instance.chat(chat).then((response) {
+        chatHistory.add(PromptChat(
+            sender: MessageSender.gemini, message: response?.output ?? ''));
+        chat.add(
+          Content(
+            parts: [Parts(text: response?.output)],
+            role: 'model',
+          ),
+        );
+      });
     }
 
-    _chatOutputController.add(ChatSuccessState(userMessage: chatHistory.last, chatHistory: chatHistory));
+    _chatOutputController.add(ChatSuccessState(
+        userMessage: chatHistory.last, chatHistory: chatHistory));
   }
 
   void dispose() {
